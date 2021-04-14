ecd872b8bafd5a74fbc26ce68def72a02f1b175e
oasis10702 <oasis10702@gmail.com>
2021-04-14 21:07:50 +0900

    add basic route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>Hi there!</h1> 
+    </div>`);
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
