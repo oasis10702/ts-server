c3db7f2f4526c6741fe4240b1a0511eb078fbf44
oasis10702 <oasis10702@gmail.com>
2021-05-25 21:59:52 +0900

    proof of concept

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,16 @@ import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 
+import { router as controllerRouter } from './controllers/decorators/controller';
+import './controllers/LoginController';
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['sddlkj'] }));
 app.use(router);
+
+app.use(controllerRouter);
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
