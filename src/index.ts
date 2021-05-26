f53ab02f3e070f8c57c6f96a4c95e96445ebc044
oasis10702 <oasis10702@gmail.com>
2021-05-26 18:05:43 +0900

    a few fixups

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 
-import { router as controllerRouter } from './controllers/decorators/controller';
+import { AppRouter } from './AppRouter';
 import './controllers/LoginController';
 
 const app = express();
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['sddlkj'] }));
 app.use(router);
 
-app.use(controllerRouter);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
