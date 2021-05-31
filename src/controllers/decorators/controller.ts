ffd9b961426186c7bceada6e4640e6da04bca75c
oasis10702 <oasis10702@gmail.com>
2021-05-31 20:38:55 +0900

    add use decorator

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -11,9 +11,10 @@ export function controller(routerPrefix: string) {
       const routerHandler = target.prototype[key];
       const path = Reflect.getMetadata(MetadataKeys.path, target.prototype, key);
       const method: Methods = Reflect.getMetadata(MetadataKeys.method, target.prototype, key);
+      const middlewares = Reflect.getMetadata(MetadataKeys.middleware, target, key) || [];
 
       if (path) {
-        router[method](`${routerPrefix}${path}`, routerHandler);
+        router[method](`${routerPrefix}${path}`, ...middlewares, routerHandler);
       }
     }
   };
