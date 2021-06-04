d5be1550c21a2f3ba9687b83e4cac3aca608aa08
oasis10702 <oasis10702@gmail.com>
2021-06-04 21:44:19 +0900

    test auto validation

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,15 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
-import { get, controller, use } from './decorators';
-
-function logger(req: Request, res: Response, next: NextFunction) {
-  console.log('Request was made!!!');
-  next();
-}
+import { get, controller, bodyValidator, post } from './decorators';
 
 @controller('/auth')
 class LoginController {
   @get('/login')
-  @use(logger)
   getLogin(req: Request, res: Response): void {
     res.send(`
         <form method="POST">
@@ -24,5 +18,18 @@ class LoginController {
           <button>Submit</button>
         </form>
       `);
+  }
+
+  @post('/login')
+  @bodyValidator('email', 'password')
+  postLogin(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    if (email && password && email === 'hi@hi.com' && password === 'password') {
+      req.session = { loggedIn: true };
+      res.redirect('/');
+    } else {
+      res.send('Invalid email or password');
+    }
   }
 }
